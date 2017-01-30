d51f1c7e9061a6f7adf80dced32aa04b6b611433
Lucas Brizolla <lbbrizolla@gmail.com>
2017-01-30 18:51:45 +0000

    Add text animation

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.texts').textillate({
+    minDisplayTime: 2000,
+    in: {effect: 'flipInY', sync: false },
+    out :{ delay: 3, effect: 'lightSpeedOut', sync: true},
+    loop: true
+
+});
+})
+
+
+
+
+
